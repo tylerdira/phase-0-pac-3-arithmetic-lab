038d4ab86670521aa005c2291c8678eeed2dd11e
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-06-15 10:22:22 -0700

    commit tree 8283cb84ef7245d556247d9509f623cb6c31f9be

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b){
+   return a + b;
+}
+
+function subtract(a, b){
+   return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function increment(n){
+    return ++n;
+}
+
+function decrement(n){
+    return --n;
+}
+
+function makeInt(n, 10){
+    return parseInt(n);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
